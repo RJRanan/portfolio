c549b9ad06ee44d987a305892330a32a4a029c07
RJRanan <155876003+RJRanan@users.noreply.github.com>
2024-04-03 16:38:19 +0800

    index

---
+++ src/sections/index.js
@@ -0,0 +1,6 @@
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import PersonalProjects from "./sections/PersonalProjects";
+import Footer from "./sections/Footer";
+
+export { Hero, About, PersonalProjects, Footer };
